d7073df6d337e75a016a1beaa1e217a78c2d62f5
David Reyes Pucheta <davidpucheta@gmail.com>
2016-12-01 11:11:59 -0600

    Solve Q number two

--- sql_evaluation.sql
+++ sql_evaluation.sql
@@ -1,3 +1,5 @@
 --1 Display the character constant 'Greeting' with a column heading of 'Hello.'
 select 'Greeting' as Hello
 
+--2 Display all data from the categories tables without specifying the names of the table columns. Order the results by CategoryName
+select * from categories order by CategoryName
