e4c3c26ed0f5c94becfa09a546cdf00fcce2054f
David Reyes Pucheta <davidpucheta@gmail.com>
2016-12-01 11:13:51 -0600

    Added question #3 answer

--- sql_evaluation.sql
+++ sql_evaluation.sql
@@ -3,3 +3,15 @@ select 'Greeting' as Hello
 
 --2 Display all data from the categories tables without specifying the names of the table columns. Order the results by CategoryName
 select * from categories order by CategoryName
+
+--3 Display all employees' names, last name first, and then first name, hire date and home phone number. Order the results by employee name, last name first, and then first name
+select 
+	lastname,
+	firstname,
+	hiredate,
+	homephone
+FROM
+	employees
+ORDER BY 
+	lastname,
+	firstname
